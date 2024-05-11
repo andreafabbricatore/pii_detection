d05e1507870ade15f9771ff7c69c10d2528f4a49
Andrea Fabbricatore <andy.fabb10@gmail.com>
2024-05-11 15:30:11 +0200

    created aux.py

---
+++ aux.py
@@ -0,0 +1,25 @@
+import json
+from datasets import Dataset
+
+def json_to_Dataset(filepath:str) -> Dataset:
+    """
+    Pass a .json filepath generated during the pipeline phase and get a Dataset file format for training and evaluation.
+    """
+
+    data = []
+    with open(filepath) as f:
+        data = json.load(f)
+
+    ids = []
+    tokens = []
+    token_ids = []
+    tokenized_bios = []
+    for i in data:
+        ids.append(i['id'])
+        tokens.append(i['tokens'])
+        token_ids.append(i['token_ids'])
+        tokenized_bios.append(i['bio_labels'])
+
+    dataset = Dataset.from_dict({'id': ids, 'tokens': tokens, 'token_ids': token_ids, 'tokenized_bios': tokenized_bios})
+
+    return dataset
