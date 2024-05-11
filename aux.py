9a727ae7608fde6e28fb7bb9b2c6b87efcbfcd00
Andrea Fabbricatore <andy.fabb10@gmail.com>
2024-05-11 16:49:15 +0200

    fixed datasets by adding source text, created inference function

--- aux.py
+++ aux.py
@@ -1,5 +1,8 @@
 import json
 from datasets import Dataset
+from transformers import AutoTokenizer
+from transformers import AutoModelForTokenClassification
+import torch
 
 def json_to_Dataset(filepath:str) -> Dataset:
     """
@@ -14,12 +17,25 @@ def json_to_Dataset(filepath:str) -> Dataset:
     tokens = []
     token_ids = []
     tokenized_bios = []
+    source_texts = []
     for i in data:
         ids.append(i['id'])
         tokens.append(i['tokens'])
         token_ids.append(i['token_ids'])
         tokenized_bios.append(i['bio_labels'])
+        source_texts.append(i['source_text'])
 
-    dataset = Dataset.from_dict({'id': ids, 'tokens': tokens, 'token_ids': token_ids, 'tokenized_bios': tokenized_bios})
+    dataset = Dataset.from_dict({'id': ids, 'tokens': tokens, 'token_ids': token_ids, 'tokenized_bios': tokenized_bios, 'source_texts':source_texts})
 
     return dataset
+
+def inference(model:AutoModelForTokenClassification, tokenizer:AutoTokenizer, text:str):
+    inputs = tokenizer(text, return_tensors="pt")
+
+    with torch.no_grad():
+        logits = model(**inputs).logits
+
+    predictions = torch.argmax(logits, dim=2)
+    predicted_token_class = [model.config.id2label[t.item()] for t in predictions[0]]
+
+    return logits, predictions, predicted_token_class
